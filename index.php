36fcedb16ff063b5876558b4f02d25d857269bfb
nwenwewinthan <41044655+NweNweWinThan777@users.noreply.github.com>
2020-06-22 08:51:06 +0630

    Add files via upload
    # add files

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+use MyApp\Game\Player\Player;
+require_once realpath("vendor/autoload.php");
+
+// $player = new Player();
+// echo "<br>";
+// $gameController = new \MyApp\Game\GameController();
+include("dist/index.html");
+?>
